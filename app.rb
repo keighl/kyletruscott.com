5b1623dde5b66b241ca5411500dc047578f49269
Kyle Truscott <kyle@thinkfa.com>
2012-01-20 10:40:45 -0500

    Reworking app structure, for more performance (singletons all around)
    Signed-off-by: Kyle Truscott <kyle@thinkfa.com>

--- app.rb
+++ app.rb
@@ -1,86 +1,94 @@
 require 'rubygems'
 require 'yaml'
 require 'haml'
-require 'sinatra'
+require 'sinatra/base'
 require 'redcarpet'
-require './blog.rb'
+#require './blog.rb'
 
 PAGE_LIMIT       = 2
 SITE_NAME        = "Keighl"
 SITE_DESCRIPTION = "#programmer #nyc"
 
-get '/' do
-  
-  blog         = Blog.new
-  @posts       = blog.prep_posts(blog.posts)
-  total        = @posts.count
-  pages        = total.fdiv(PAGE_LIMIT).ceil
-  page         = 1
-  @prev        = nil
-  @next        = (pages == page) ? nil : page + 1;
-  @posts       = @posts[0, PAGE_LIMIT]
-  @title       = SITE_NAME
-  @description = SITE_DESCRIPTION
-  
-  headers 'X-Hacker' => "annyeong."
+class Keighl < Sinatra::Base
 
-  erb :archive
+  configure do |c|
 
-end
+    # Bootstrap
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, :hard_wrap => true, :autolink => true, :no_intraemphasis => true, :fenced_code => true)
+    posts    = YAML::load(File.read("content/manifest.yml"))
+    total    = posts.count
+    pages    = total.fdiv(PAGE_LIMIT).ceil
 
-get %r{/page/([\d]+)} do
-  
-  blog  = Blog.new
-  @posts = blog.prep_posts(blog.posts)
-  total  = @posts.count
-  pages  = total.fdiv(PAGE_LIMIT).ceil
-  page   = params[:captures].first.to_i
-  offset = (page - 1) * PAGE_LIMIT
-  @posts = @posts[offset, PAGE_LIMIT]
-  
-  if @posts.empty?
-    halt 404, erb(:not_found)
+    # Markdown
+    posts.each do |post|
+      content        = File.read("content/#{post[:slug].to_s}.md")
+      post[:content] = markdown.render(content)
+      post[:date]    = Time.parse(post[:date])
+    end
+
+    set :posts, posts
+    set :pages, total.fdiv(PAGE_LIMIT).ceil
+    set :total, posts.count
+
   end
-  
-  @prev        = (page == 1) ? nil : page - 1
-  @next        = (pages == page) ? nil : page + 1;
-  @title       = "Page #{page} | #{SITE_NAME}"
-  @description = SITE_DESCRIPTION
-  
-  headers 'X-Hacker' => "annyeong."
-  
-  erb :archive
-  
-end
 
-get '/post/:slug/?' do
-  
-  blog  = Blog.new
-  @post = blog.posts.find { |post| post[:slug] == params[:slug] }
-  
-  headers 'X-Hacker' => "annyeong."
+  get '/' do
+    page         = 1
+    @prev        = nil
+    @next        = (settings.pages == page) ? nil : page + 1;
+    @posts       = settings.posts[0, PAGE_LIMIT]
+    @title       = SITE_NAME
+    @description = SITE_DESCRIPTION
 
-  if @post
-    @post        = blog.prep_post(@post)
-    @title       = "#{@post[:title]} | #{SITE_NAME}"
-    @description = @post[:description]
-    erb :post
-  else
-    halt 404, erb(:not_found)
+    headers 'X-Hacker' => "annyeong."
+
+    erb :archive
   end
-  
-end
 
-get '/feed' do
-  blog  = Blog.new
-  @posts = blog.posts
-  @posts = @posts[0, 10]
-  @posts = blog.prep_posts(@posts)
-  content_type 'application/rss+xml'
-  haml(:rss, :format => :xhtml, :escape_html => true, :layout => false)
-end
+  get %r{/page/([\d]+)} do
+    page   = params[:captures].first.to_i
+    offset = (page - 1) * PAGE_LIMIT
+    @posts = settings.posts[offset, PAGE_LIMIT]
+    if @posts.empty?
+      halt 404, erb(:not_found)
+    end
+    @prev        = (page == 1) ? nil : page - 1
+    @next        = (settings.pages == page) ? nil : page + 1;
+    @title       = "Page #{page} | #{SITE_NAME}"
+    @description = SITE_DESCRIPTION
 
-not_found do
-  headers 'X-Hacker' => "annyeong."
-  erb :not_found
+    headers 'X-Hacker' => "annyeong."
+
+    erb :archive
+  end
+
+  get '/post/:slug/?' do
+
+    @post = settings.posts.find { |post| post[:slug] == params[:slug] }
+
+    headers 'X-Hacker' => "annyeong."
+
+    if @post
+      @title       = "#{@post[:title]} | #{SITE_NAME}"
+      @description = @post[:description]
+      erb :post
+    else
+      halt 404, erb(:not_found)
+    end
+
+  end
+
+  get '/feed' do
+    @posts = settings.posts[0, 10]
+    content_type 'application/rss+xml'
+    haml(:rss, :format => :xhtml, :escape_html => true, :layout => false)
+  end
+
+  not_found do
+    headers 'X-Hacker' => "annyeong."
+    erb :not_found
+  end
+
+  run! if app_file == $0
+
 end
--- config.ru
+++ config.ru
@@ -1,10 +1,9 @@
 require './app.rb'
- 
+
 root_dir = File.dirname(__FILE__)
- 
+
 set :environment, ENV['RACK_ENV'].to_sym
 set :root,        root_dir
 set :app_file,    File.join(root_dir, 'app.rb')
-disable :run
- 
-run Sinatra::Application
+
+run Keighl
