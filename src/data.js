bbcaf384c860095fcd20f73acb9c3b3a004e57be
Kyle Truscott <me@keighl.com>
2018-06-26 11:07:49 -0400

    Update recent work

--- src/data.js
+++ src/data.js
@@ -29,8 +29,8 @@ export default {
   },
   technology: [
     'golang',
-    'vuejs',
     'react',
+    'vuejs',
     'ruby',
     'graphql',
     'nodejs',
@@ -39,10 +39,25 @@ export default {
   ],
   recent_work: [
     {
+      name: 'eBay',
+      desc: 'Develop design tools to normalize brand standards across the vast universe of eBay design projects',
+      role: 'Engineering Lead',
+      tasks: [
+        'development',
+        'prototyping',
+        'technical-strategy'
+      ],
+      technology: [
+        'react',
+        'nodejs'
+      ]
+    },
+    {
       name: 'Crate Space',
       url: 'https://www.cratespace.com',
-      desc: 'Save on name-brand household products by building your own bundle',
-      roles: [
+      desc: 'Lead product design and development at NY based e-commerce startup. Implemented unique bundle-based UX for household essentials.',
+      role: 'CTO',
+      tasks: [
         'design',
         'development',
         'logistics'
@@ -55,11 +70,18 @@ export default {
     },
     {
       name: 'Google OnHub + IFTTT',
+      role: 'Creative Technologist',
       url: 'http://youtu.be/JPH74ZHDuCI',
-      desc: 'Partnered with Google to showcase their OnHub/IFTTT integration',
-      roles: [
+      desc: 'Partnered with Google to showcase their OnHub/IFTTT integration by prototyping creative connections between the router and connected-home appliances. Filmed over two days in Brooklyn.',
+      tasks: [
         'hacking',
         'acting!'
+      ],
+      technology: [
+        'golang',
+        'android',
+        'google-app-engine',
+        'iot-apis'
       ]
     }
   ]
