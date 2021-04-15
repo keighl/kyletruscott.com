38d7e097d1e1b069712218a10ce7929fce842611
Kyle Truscott <kyle@freeassociation.is>
2021-04-15 16:13:27 -0400

    Add 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,11 +1,39 @@
 /** @jsx jsx */
-import { jsx } from '@emotion/react'
+import { jsx, ThemeProvider } from '@emotion/react'
 import { FunctionComponent } from 'react'
+import { PageProps } from 'gatsby'
 
-type Props = {}
+import { media, themes } from '../style'
+import Tree from '../components/Tree'
+import '../style/main.scss'
 
-const NotFoundPage: FunctionComponent<Props> = ({}) => {
-	return <div></div>
+type Props = PageProps & {}
+
+const NotFoundPage: FunctionComponent<Props> = () => {
+	return (
+		<ThemeProvider theme={themes.dark}>
+			<div
+				css={{
+					padding: '1.5rem',
+					[media.mediumUp]: {
+						padding: '7rem 1.5rem',
+					},
+				}}
+			>
+				<div
+					css={{
+						margin: '0 auto',
+						[media.mediumUp]: {
+							padding: '0 4rem',
+							maxWidth: '60rem',
+						},
+					}}
+				>
+					<Tree data={{ status: { code: 404, message: 'Not found' } }} />
+				</div>
+			</div>
+		</ThemeProvider>
+	)
 }
 
 export default NotFoundPage
